f40c2ceb893d2ad63e2576f4114c45eb870a340f
Denis Michailov <kamrad.reg@gmail.com>
2016-07-11 22:16:34 +1000

    Scaling

--- src/js/app.js
+++ src/js/app.js
@@ -7,9 +7,17 @@ d3.select('.d3-here').append('p').text('Hello, this is D3 practice start point.'
 
 var width = 400;
 var height = 400;
-var barHeight = 50;
+var barHeight = 10;
 
-var dataArray = [10, 30, 450];
+var dataArray = [10, 30, 450, 1200, 244, 100, 12, 460, 20];
+
+var widthScale = d3.scaleLinear()
+	.domain([0, 1200])
+	.range([0, width]);
+
+var colorScale = d3.scaleLinear()
+	.domain([0, 1200])
+	.range(['#333', '#777']);
 
 var canvas = d3.select('.d3-here')
 	.append('svg')
@@ -21,10 +29,12 @@ var bars = canvas.selectAll('rect')
 	.enter()
 		.append('rect')
 		.attr('width', function(d, i) {
-			return d;
+			return widthScale(d);
+		})
+		.attr('fill', function(d, i) {
+			return colorScale(d);
 		})
 		.attr('height', barHeight)
 		.attr('y', function(d, i) {
-			console.log(d);
 			return i * barHeight * 1.2;
 		});
