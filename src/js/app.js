07a6edd3d182bc34e84532ab65c7077ae1be610e
Denis Michailov <denis.michailov@gmail.com>
2016-07-12 16:28:36 +1000

    Axis

--- src/js/app.js
+++ src/js/app.js
@@ -3,26 +3,38 @@ var $c_01 = '#21b1e0',
 	$c_03 = '#1d7e6b';
 
 console.log( d3.version );
-d3.select('.d3-here').append('p').text('Hello, this is D3 practice start point.');
+d3.select('.d3-here').append('p').text('Hello, this is D3 practice.');
+
+var dataArray = [10, 30, 450, 1200, 244, 100, 12, 460, 20, 230, 490];
 
 var width = 400;
-var height = 400;
+var height = dataArray.length * 10 + 100;
 var barHeight = 10;
 
-var dataArray = [10, 30, 450, 1200, 244, 100, 12, 460, 20];
 
 var widthScale = d3.scaleLinear()
 	.domain([0, 1200])
-	.range([0, width]);
+	.range([0, width-50]);
 
 var colorScale = d3.scaleLinear()
 	.domain([0, 1200])
 	.range(['#333', '#777']);
 
+var axis = d3.axisTop(widthScale)
+	.ticks(5);
+
 var canvas = d3.select('.d3-here')
 	.append('svg')
 	.attr('width', width)
-	.attr('height', height);
+	.attr('height', height)
+	.append('g')
+	.attr('transform', 'translate(10, ' + barHeight * 5 + ')');
+
+canvas.append('g')
+	.attr('transform', 'translate(0, -20)')
+	.attr('class', 'axis')
+	.call(axis);
+
 
 var bars = canvas.selectAll('rect')
 	.data(dataArray)
