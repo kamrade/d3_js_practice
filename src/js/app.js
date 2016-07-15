54ba6ea4ec350c7df8721434118c9050a7f00573
Denis Michailov <denis.michailov@gmail.com>
2016-07-15 17:50:09 +1000

    Randomize values in graph

--- src/js/app.js
+++ src/js/app.js
@@ -1,35 +1,45 @@
+// drek
+console.log( "d3 version is " + d3.version );
+d3.select('.d3-here').append('p').text('Hello, this is D3 practice.');
+
+// templates
+var nodesQ = 0;
+var nodes = [];
+var links = [];
+
+//  Options
 var $c_01 = '#21b1e0',
 	$c_02 = '#1abc9c',
 	$c_03 = '#1d7e6b';
+var maxValues = 20;
+var minValues = 4;
+var maxValY = 400;
+var maxY = 400;
+var minY = 50;
+var width = 600;
+var height = 400;
+var nodeRadius = 5;
+var gap = 2;
+var lineWidth = 3;
+var ticksX = 2;
+var ticksY = 2;
 
-console.log( d3.version );
-d3.select('.d3-here').append('p').text('Hello, this is D3 practice.');
+// Logic
+function randomNodes() {
+	nodesQ = Math.floor(Math.random() * (maxValues - minValues)) + minValues;
+	ticksX = nodesQ - 1;
+	console.log(nodesQ);
+	var item = { x: 0, y: 0 };
+	for (var i = 0; i < nodesQ; i++) {
+		item.x = i * 50;
+		item.y = Math.floor(Math.random() * (maxY - minY)) + minY;
+		nodes.push({ x: item.x, y: item.y });
+	}
+}
 
-var nodes = [
-	{x: 0, y: 50},
-	{x: 50, y: 80},
-	{x: 100, y: 120},
-	{x: 150, y: 150},
-	{x: 200, y: 80},
-	{x: 250, y: 120},
-	{x: 300, y: 9},
-	{x: 350, y: 300},
-	{x: 400, y: 310},
-	{x: 450, y: 100},
-	{x: 500, y: 380},
-	{x: 550, y: 230},
-	{x: 600, y: 280},
-	{x: 650, y: 440},
-	{x: 700, y: 230},
-	{x: 750, y: 28},
-	{x: 800, y: 230},
-	{x: 850, y: 230},
-	{x: 900, y: 30},
-	{x: 950, y: 60},
-	{x: 1000, y: 10}
-];
+randomNodes();
 
-var links = [];
+
 for(var i = 0; i < (nodes.length - 1); i++) {
 	links.push({
 		source: nodes[i],
@@ -37,19 +47,23 @@ for(var i = 0; i < (nodes.length - 1); i++) {
 	});
 }
 
-var width = 1200;
-var height = 600;
-var gap = 4;
-var lineWidth = 3;
-
+// масштабирование
 var widthScale = d3.scaleLinear()
 	.domain([0, nodes[nodes.length - 1].x ])
 	.range([0 , width - 20]);
 
-console.log( nodes[nodes.length - 1].x );
+var heightScale = d3.scaleLinear()
+	.domain([0, maxValY])
+	.range([0, height - 100]);
 
-var axis = d3.axisBottom(widthScale);
+// создаем линейки
+var axis = d3.axisBottom(widthScale)
+	.ticks(ticksX);
 
+var axisRight = d3.axisRight(heightScale)
+	.ticks(ticksY);
+
+// прицепляемся к элементу
 var canvas = d3.select('.d3-here')
 	.append('svg')
 	.attr('width', width + 50)
@@ -57,10 +71,7 @@ var canvas = d3.select('.d3-here')
 	.append('g')
 	.attr('transform', 'translate(10, 10)');
 
-
-canvas.text('The Graph')
-	.select('.d3-here');
-
+// фоновые rect-ы
 canvas.append('g').attr('class', 'svg-rects-bg').selectAll('.rect')
 	.data(links)
 	.enter()
@@ -73,36 +84,47 @@ canvas.append('g').attr('class', 'svg-rects-bg').selectAll('.rect')
 		.attr('fill', $c_02)
 		.attr('opacity', '.1');
 
-
+// линии графика
 canvas.append('g').attr('class', 'svg-lines').selectAll('.line')
 	.data(links)
 	.enter()
 	.append('line')
 	.attr('x1', function(d) { return widthScale(d.source.x); })
-	.attr('y1', function(d) { return d.source.y; })
+	.attr('y1', function(d) { return heightScale(d.source.y); })
 	.attr('x2', function(d) { return widthScale(d.target.x); })
-	.attr('y2', function(d) { return d.target.y; })
+	.attr('y2', function(d) { return heightScale(d.target.y); })
 	.style('stroke', $c_03)
 	.attr('stroke-width', lineWidth);
 
+// точки
 canvas.append('g').attr('class', 'svg-nodes').selectAll('circle .nodes')
 	.data(nodes)
 	.enter()
 		.append('svg:circle')
 		.attr('class', 'nodes')
 		.attr('cx', function(d) { return widthScale(d.x); })
-		.attr('cy', function(d) { return d.y; })
-		.attr('r', 5)
+		.attr('cy', function(d) { return heightScale(d.y); })
+		.attr('r', nodeRadius)
 		.attr('fill', $c_01)
 		.attr('stroke', '#fff')
 		.attr('stroke-width', gap);
 
-
+// прицепляем линейки - внизу и справа
 canvas.append('g')
 	.attr('transform', 'translate(0,' + (height - 90) + ')')
-	.attr('class', 'axis')
+	.attr('class', 'axisX')
 	.call(axis);
 
+canvas.append('g')
+	.attr('transform', 'translate(' + width + ', 0)')
+	.attr('class', 'axisY')
+	.call(axisRight);
+
+// interaction ...
 $('.svg-nodes circle').on('click', function(){
-	console.log('click');
+	console.log('node click');
+});
+
+$('.svg-rects-bg rect').on('click', function(){
+	console.log('rect click');
 });
